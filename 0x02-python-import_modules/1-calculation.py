48cb434a01aa41994804b6737dbd721599e906c8
1MAGNOVA <olalekanabsm@gmail.com>
2023-08-11 18:43:33 -0700

    1-calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,8 +2,9 @@
 """a = 10
 b = 5"""
 if __name__ == "__main__":
-     = 10
+    a = 10
     b = 5
+    from calculator_1 import add, sub, mul, div
 print("{} + {} = {}".format(a, b, add(a, b)))
 print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
