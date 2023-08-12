4ad82cf48753286cbb66715684491c2541a5d752
1MAGNOVA <olalekanabsm@gmail.com>
2023-08-11 18:01:27 -0700

    0-add

---
+++ 0x02-python-import_modules/o-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""imports the def add(a, b): from 0-add.py abd prints result of addition"""
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add 
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b))
