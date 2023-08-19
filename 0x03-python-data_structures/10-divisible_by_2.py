9637ba893f4a14b376e55fad2d65230c1214c19e
1MAGNOVA <olalekanabsm@gmail.com>
2023-08-18 19:33:50 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     multiples = []
-    for i in range(len(my_list)):
-        if (my_list[i]) % 2 == 0:
+    for i in range(my_list):
+        if my_list[i] % 2 == 0:
             return nultiples.append(True)
         else:
            returnmultiples.append(False)
