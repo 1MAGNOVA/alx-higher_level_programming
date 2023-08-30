45674c427331556c23862112af87caed7aafae05
1MAGNOVA <olalekanabsm@gmail.com>
2023-08-29 20:17:45 -0700

    magic

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except(result != result):
+        except():
             result = b + a
             break
     return (result)
