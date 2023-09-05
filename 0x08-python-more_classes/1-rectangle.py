8c8c878194e271c232dd325576e438e058ae45e2
1MAGNOVA <olalekanabsm@gmail.com>
2023-09-05 10:27:50 -0700

    1-rect

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-class Rectangle
+"""defines a class rectangle with  attributes width, height"""
 
 
-def __init__(self, width=0, height=0):
-    self.width = width
-    self.height = height
+class Rectangle:
+    """ contains attributes: width, height as @properties"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
     def width(self):
         return self.__width
