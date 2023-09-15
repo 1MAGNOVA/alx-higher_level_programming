6a97ec917e82fa78dacf90d1faddde8e8eddcf0c
1MAGNOVA <olalekanabsm@gmail.com>
2023-09-14 19:48:00 -0700

    base geometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+===================================
+module using class BaseGeometry
+===================================
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+===================================
+module using class BaseGeometry
+===================================
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    @classmethod
+    def area(self):
+        """method for area calculated """
+
+        raise Exception("area() not implemented")
